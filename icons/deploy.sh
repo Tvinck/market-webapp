3e28eeedd6214aaf233786cb4f2326ab1384a7ab
Tvinck <112720884+Tvinck@users.noreply.github.com>
2025-08-09 16:39:11 +0300

    Delete icons directory

--- icons/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Deploy web assets and server API
-set -e
-RSYNC_DEST=${RSYNC_DEST:-user@host:/var/www/marker-webapp}
-rsync -av --delete \
-  index.html app.js styles.css src icons server \
-  "$RSYNC_DEST/"
-
